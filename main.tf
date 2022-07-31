21fa98cfa6f9c525ce09bbecd1fc8407621bac1d
olufolarin <folami25@gmail.com>
2022-07-31 20:24:40 +0100

    duplication of files

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 terraform {
-<<<<<<< HEAD
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,19 +10,6 @@ terraform {
 // configure the provider: aws
 provider "aws" {
   region = "us-east-1"
-}
-=======
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
-}
-
-# configure the aws provider
-provider "aws" {
-    region = "us-east-1"
 }
 
 #create an s3 bucket using terraform: resource, resource name and internal tf name
@@ -50,14 +36,3 @@ resource "aws_s3_bucket_versioning" "my_s3_versioning" {
   }
 }
 
-# output
-output "my_s3_bucket_versioning" {
-  value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
-}
-
-# iam user config deets output
-output "iam_user_config_deets" {
-  value = aws_iam_user.my_iam_user
-}
-# NOTE: AWS Resource Notation (arn) is used to uniquely identify an aws resource
->>>>>>> ed3923d (a recap of basic terraform resources)
